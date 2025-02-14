f8a2c16b6177477ccedd038f185df83b7ac177c4
JosueFE10 <ch220111047@chapala.tecmm.edu.mx>
2025-02-14 01:30:32 -0600

    Add files via upload

---
+++ spript.js
@@ -0,0 +1,7 @@
+document.getElementById("carta").addEventListener("mouseenter", function() {
+    console.log("Carta abierta 🎉");
+});
+
+document.getElementById("carta").addEventListener("mouseleave", function() {
+    console.log("Carta cerrada 💌");
+});
